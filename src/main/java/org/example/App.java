559a0bed254c99af692cbd85cda1fdbf0a21cef1
nehru <nehrusathappan@gmail.com>
2025-07-06 15:20:54 -0700

    created new add function

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,5 +5,13 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int ans = add(4,5);
+        System.out.println(ans);
     }
+
+    static int add(int x, int y){
+        System.out.println("adding sum x and y");
+        return x+y;
+    }
+
 }
